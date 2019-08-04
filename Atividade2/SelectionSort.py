db9a70222ce0449f0ea1b7aef7db467d2d6dfdf0
Davi Silva <51710576+DaviSilva435@users.noreply.github.com>
2019-08-03 22:52:22 -0300

    Update SelectionSort.py

--- Atividade2/SelectionSort.py
+++ Atividade2/SelectionSort.py
@@ -2,46 +2,66 @@ from random import randint
 import timeit
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+	
+  g1,g2 =[], []
   
-def geraLista(tam):
-    lista = []
-    for i in range(tam):
-        n = randint(1,1*tam)
-        if n not in lista: lista.append(n)
-    return lista
-  
-mpl.use('Agg')
-
-def SelectionSort(lista):
-  for i in range(len(lista)): 
-    indexadorMinimo=i 
-    for j in range(i+1, len(lista)): 
-        if lista[indexadorMinimo]>lista[j]: 
-            indexadorMinimo=j 
-    aux=lista[i]
-    lista[i]=lista[indexadorMinimo]
-    lista[indexadorMinimo]=aux
-  return lista
-
-print(SelectionSort(geraLista(30)))
-  
-def desenhaGrafico(x,y,y2,xl = "Entradas", yl = "Saídas"):
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111)
-    ax.plot(x,y, label = "Melhor Tempo. Requisições x Tempo")
-    ax.plot(x,y2, label = "Numero de Swaps. Requisições x Nº Operações")
-    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
-    plt.ylabel(yl)
-    plt.xlabel(xl)
-    fig.savefig('graph.png')
-   
-x2 = [100,200,300,400]
-y = []
-y2= []
-  
-for i in range(4):
-    y.append(timeit.timeit("SelectionSort({})".format(x2[i]),setup="from __main__ import SelectionSort",number=1))  
-  
-    y2.append(timeit.timeit("SelectionSort({})".format(x2[i]),setup="from __main__ import SelectionSort",number=1)) 
-  
-desenhaGrafico(x2,y,y2)
+	def geravetor1(tam):
+	    vetor = []
+	    while tam:
+	        vetor.append(tam)
+	        tam-=1
+	    return vetor
+    
+  def OrdenaSS1(vetor):
+	    count=0
+	    for i in range(len(vetor)):
+	        k = i
+	        for j in range(i+1, len(vetor)):
+	            if vetor[k] > vetor[j]:
+	                k = j
+	            if vetor[i] != vetor[k]:
+	                vetor[k], vetor[i] = vetor[i], vetor[k]
+	                count+=1
+	    g1.append(count)
+	
+	def geravetor2(tam):
+	    vetor = []
+	    for i in range(tam):
+	        numero = randint(1,1*tam)
+	        if numero not in vetor: vetor.append(numero)
+	    return vetor
+    
+	def OrdenaSS2(vetor):
+	    count=0
+	    for i in range(len(vetor)):
+	        k = i
+	        for j in range(i+1, len(vetor)):
+	            if vetor[k] > vetor[j]:
+	                k = j
+	            if vetor[i] != vetor[k]:
+	                vetor[k], vetor[i] = vetor[i], vetor[k]
+	                count+=1
+	    g2.append(count)
+	
+	def desenhaGrafico(x,vetor1,vetor2,xl = "Entradas", yl = "Y"):
+	    fig = plt.figure(figsize=(10, 8))
+	    ax = fig.add_subplot(111)
+	    ax.plot(x, vetor1, label = "Aleatória")
+	    ax.plot(x, vetor2, label = "Invertida")
+	    ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+	    plt.ylabel(yl)
+	    plt.xlabel(xl)
+	    plt.savefig('PlotagemTempo.png')
+	
+	quant = [10000, 20000, 50000, 100000]
+	modeloG1 = []
+	modeloG2 = []
+	for i in range(len(quant)):
+	    print(quant[i])
+	    vetor1 = geraVetor(quant[i])
+	    vetor2 = geraVetorInvertida(quant[i])
+	    modeloG1.append(timeit.timeit("OrdenaSS1({})".format(vetor1),setup="from __main__ import OrdenaSS1",number=1))
+	    modeloG2.append(timeit.timeit("OrdenaSS2({})".format(vetor2), setup="from __main__ import OrdenaSS2", number=1))
+	
+	desenhaGrafico(quant,modeloG1,modeloG2,"Tamanho", "Tempo", "saida_time")
+	desenhaGrafico(quant,g1,g2, "Tamanho", "Operacoes", "saida_operacoes")
